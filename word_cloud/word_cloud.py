1bcf2b0dfaea6627dfd6b517160d6393f15ca2be
YuFeng Lin <a382259104@gmail.com>
2024-03-04 10:27:15 -0500

    small bug fix

--- word_cloud/word_cloud.py
+++ word_cloud/word_cloud.py
@@ -2,31 +2,34 @@ from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 import json
 
+# https://amueller.github.io/word_cloud/auto_examples/index.html
 
 max_words = 1000
 
-def makeImage(frequencies):
+def makeImage(products):
     wc = WordCloud(background_color="white", max_words=max_words)
 
     # generate word cloud
-    wc.generate_from_frequencies(frequencies)
+    for product_name,product in products.items():
+        wc.generate_from_frequencies(product)
 
-    # show
-    plt.imshow(wc, interpolation="bilinear")
-    plt.axis("off")
+        # show
+        plt.figure()
+        plt.imshow(wc, interpolation="bilinear")
+        plt.title(product_name)
+        plt.axis("off")
+        
     plt.show()
 
+    
 def parseFrequencies():
     """given product_keywords.json is in the same directory as this script"""
     
-    reviews = {}
+    products = {}
     
     with open("product_keywords.json", 'r') as file:
-        products = json.load(file)
-        r = products.get('Product One')
-        print(r)
-        reviews = r
-
+        p = json.load(file)
+        products = p
    
     """
      Let's say you have a total weight of 100
@@ -35,15 +38,15 @@ def parseFrequencies():
     then 10% from the total words percentage * max_words = 10% * 1000
     """
     
-    total = sum(weights for weights in reviews.values())
-    
-    for key in reviews:
-        percentage = reviews[key] / total
-        frequency = int(percentage * max_words)
-        reviews[key] = frequency
-        
-    print(reviews)        
-    return reviews
+    for reviews in products.values():
+        total = sum(weights for weights in reviews.values())
+        for key in reviews:
+            percentage = reviews[key] / total
+            frequency = int(percentage * max_words)
+            reviews[key] = frequency
+            
+    print(products)
+    return products
 
-parseFrequencies()
+# parseFrequencies()
 makeImage(parseFrequencies())
